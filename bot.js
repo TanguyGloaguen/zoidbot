83f7bbc4b90b422447db88cd7e2a323c930b6f5a
Rémi Even <even.remi@gmail.com>
2016-02-13 20:35:18 +0100

    First commit

---
+++ bot.js
@@ -0,0 +1,15 @@
+var Botkit = require('botkit');
+
+var config = require(__dirname + '/config.js');
+
+var controller = Botkit.slackbot({
+	debug: false
+});
+
+controller.spawn({
+	token: config.token
+}).startRTM();
+
+controller.hears("hello", "direct_message", function(bot, message) {
+	bot.reply(message, "Hello from the Zoid side !");
+});
