940f7eafbc7dfbdd705e8f5ff063d83c8b7dbe8f
Rémi Even <even.remi@gmail.com>
2016-02-13 21:57:42 +0100

    Broader range of messages leading to greetings

--- bot.js
+++ bot.js
@@ -10,6 +10,6 @@ controller.spawn({
 	token: config.token
 }).startRTM();
 
-controller.hears("hello", "direct_message", function(bot, message) {
+controller.hears("hello", "direct_message,direct_mention,mention", function(bot, message) {
 	bot.reply(message, "Hello from the Zoid side !");
 });
